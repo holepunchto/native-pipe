67ccc861cd89b285f822f5ca1072b33b00e71b43
Mathias Buus <mathiasbuus@gmail.com>
2023-02-21 20:20:38 +0100

    working

--- binding.c
+++ binding.c
@@ -1,53 +1,314 @@
 #include <napi-macros.h>
 #include <node_api.h>
 #include <uv.h>
+#include <stdlib.h>
 
-#include <stdio.h>
+typedef struct {
+  uv_pipe_t pipe;
+  uv_connect_t conn;
+  uv_buf_t read_buf;
+  uv_shutdown_t end;
+  napi_env env;
+  napi_ref ctx;
+  napi_ref on_connect;
+  napi_ref on_write;
+  napi_ref on_end;
+  napi_ref on_read;
+  napi_ref on_close;
+} native_pipe_t;
+
+static void
+on_connect (uv_connect_t *req, int status) {
+  native_pipe_t *self = (native_pipe_t *) req->data;
+  napi_env env = self->env;
+
+  napi_handle_scope scope;
+  napi_open_handle_scope(env, &scope);
+
+  napi_value ctx;
+  napi_get_reference_value(env, self->ctx, &ctx);
+  napi_value callback;
+  napi_get_reference_value(env, self->on_connect, &callback);
+  napi_value argv[1];
+  napi_create_int32(env, status, &(argv[0]));
+
+  if (napi_make_callback(env, NULL, ctx, callback, 1, argv, NULL) == napi_pending_exception) {
+    napi_value fatal_exception;
+    napi_get_and_clear_last_exception(env, &fatal_exception);
+    napi_fatal_exception(env, fatal_exception);
+  }
+
+  napi_close_handle_scope(env, scope);
+}
 
 static void
 on_write (uv_write_t *req, int status) {
-  printf("wrote!\n");
+  native_pipe_t *self = (native_pipe_t *) req->data;
+  napi_env env = self->env;
+
+  napi_handle_scope scope;
+  napi_open_handle_scope(env, &scope);
+
+  napi_value ctx;
+  napi_get_reference_value(env, self->ctx, &ctx);
+  napi_value callback;
+  napi_get_reference_value(env, self->on_write, &callback);
+  napi_value argv[1];
+  napi_create_int32(env, status, &(argv[0]));
+
+  if (napi_make_callback(env, NULL, ctx, callback, 1, argv, NULL) == napi_pending_exception) {
+    napi_value fatal_exception;
+    napi_get_and_clear_last_exception(env, &fatal_exception);
+    napi_fatal_exception(env, fatal_exception);
+  }
+
+  napi_close_handle_scope(env, scope);
 }
 
-NAPI_METHOD(pipe_init) {
-  NAPI_ARGV(1)
+static void
+on_shutdown (uv_shutdown_t *req, int status) {
+  native_pipe_t *self = (native_pipe_t *) req->data;
+  napi_env env = self->env;
+
+  napi_handle_scope scope;
+  napi_open_handle_scope(env, &scope);
+
+  napi_value ctx;
+  napi_get_reference_value(env, self->ctx, &ctx);
+  napi_value callback;
+  napi_get_reference_value(env, self->on_end, &callback);
+  napi_value argv[1];
+  napi_create_int32(env, status, &(argv[0]));
+
+  if (napi_make_callback(env, NULL, ctx, callback, 1, argv, NULL) == napi_pending_exception) {
+    napi_value fatal_exception;
+    napi_get_and_clear_last_exception(env, &fatal_exception);
+    napi_fatal_exception(env, fatal_exception);
+  }
+
+  napi_close_handle_scope(env, scope);
+}
+
+static void
+on_read (uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
+  if (nread == UV_EOF) nread = 0;
+  else if (nread == 0) return;
+
+  native_pipe_t *self = (native_pipe_t *) stream;
+  napi_env env = self->env;
+
+  napi_handle_scope scope;
+  napi_open_handle_scope(env, &scope);
+
+  napi_value ctx;
+  napi_get_reference_value(env, self->ctx, &ctx);
+  napi_value callback;
+  napi_get_reference_value(env, self->on_read, &callback);
+  napi_value argv[1];
+  napi_create_int32(env, nread, &(argv[0]));
+
+  if (napi_make_callback(env, NULL, ctx, callback, 1, argv, NULL) == napi_pending_exception) {
+    napi_value fatal_exception;
+    napi_get_and_clear_last_exception(env, &fatal_exception);
+    napi_fatal_exception(env, fatal_exception);
+  }
+
+  napi_close_handle_scope(env, scope);
+}
+
+static void
+on_close (uv_handle_t *handle) {
+  native_pipe_t *self = (native_pipe_t *) handle;
+  napi_env env = self->env;
+
+  napi_handle_scope scope;
+  napi_open_handle_scope(env, &scope);
+
+  napi_value ctx;
+  napi_get_reference_value(env, self->ctx, &ctx);
+  napi_value callback;
+  napi_get_reference_value(env, self->on_close, &callback);
+
+  if (napi_make_callback(env, NULL, ctx, callback, 0, NULL, NULL) == napi_pending_exception) {
+    napi_value fatal_exception;
+    napi_get_and_clear_last_exception(env, &fatal_exception);
+    napi_fatal_exception(env, fatal_exception);
+  }
+
+  napi_close_handle_scope(env, scope);
+}
+
+static void
+on_alloc (uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+  native_pipe_t *self = (native_pipe_t *) handle;
+  *buf = self->read_buf;
+}
+
+NAPI_METHOD(native_pipe_init) {
+  NAPI_ARGV(8)
 
   uv_loop_t *loop;
   napi_get_uv_event_loop(env, &loop);
 
-  NAPI_ARGV_BUFFER_CAST(uv_pipe_t *, handle, 0);
-  uv_pipe_init(loop, handle, 0);
+  NAPI_ARGV_BUFFER_CAST(native_pipe_t *, self, 0)
+  uv_pipe_init(loop, &(self->pipe), 0);
+
+  NAPI_ARGV_BUFFER(read_buf, 1)
+
+  self->read_buf.base = read_buf;
+  self->read_buf.len = read_buf_len;
+
+  napi_create_reference(env, argv[2], 1, &(self->ctx));
+  napi_create_reference(env, argv[3], 1, &(self->on_connect));
+  napi_create_reference(env, argv[4], 1, &(self->on_write));
+  napi_create_reference(env, argv[5], 1, &(self->on_end));
+  napi_create_reference(env, argv[6], 1, &(self->on_read));
+  napi_create_reference(env, argv[7], 1, &(self->on_close));
+
+  self->env = env;
 
   return NULL;
 }
 
-NAPI_METHOD(pipe_open) {
+NAPI_METHOD(native_pipe_connect) {
   NAPI_ARGV(2)
 
-  NAPI_ARGV_BUFFER_CAST(uv_pipe_t *, handle, 0);
-  NAPI_ARGV_INT32(fd, 1);
+  NAPI_ARGV_BUFFER_CAST(native_pipe_t *, self, 0)
+  NAPI_ARGV_UTF8(path, 4096, 1)
 
-  uv_pipe_open(handle, fd);
+  uv_connect_t *conn = &(self->conn);
+
+  conn->data = self;
+  uv_pipe_connect(conn, (uv_pipe_t *) self, path, on_connect);
+
+#ifdef WIN32
+  NAPI_RETURN_INT32(-1)
+#else
+  uv_os_fd_t fd;
+  uv_fileno((uv_handle_t *) self, &fd);
+
+  NAPI_RETURN_INT32(fd);
+#endif
+}
+
+NAPI_METHOD(native_pipe_open) {
+  NAPI_ARGV(2)
+
+  NAPI_ARGV_BUFFER_CAST(uv_pipe_t *, self, 0)
+  NAPI_ARGV_UINT32(fd, 1)
+
+  uv_pipe_open(self, fd);
 
   return NULL;
 }
 
-NAPI_METHOD(pipe_write) {
+NAPI_METHOD(native_pipe_writev) {
   NAPI_ARGV(3)
 
-  NAPI_ARGV_BUFFER_CAST(uv_write_t *, req, 0);
-  NAPI_ARGV_BUFFER_CAST(uv_stream_t *, handle, 1);
-  NAPI_ARGV_BUFFER(buf, 2);
+  NAPI_ARGV_BUFFER_CAST(uv_write_t *, req, 0)
+  NAPI_ARGV_BUFFER_CAST(uv_stream_t *, handle, 1)
 
-  uv_buf_t bufs = {
-    .base = buf,
-    .len = buf_len
-  };
+  napi_value arr = argv[2];
+  napi_value item;
 
-  uv_write(req, handle, &bufs, 1, on_write);
+  uint32_t nbufs;
+  napi_get_array_length(env, arr, &nbufs);
+
+  uv_buf_t *bufs = malloc(sizeof(uv_buf_t) * nbufs);
+
+  for (uint32_t i = 0; i < nbufs; i++) {
+    napi_get_element(env, arr, i, &item);
+    uv_buf_t *buf = &(bufs[i]);
+    napi_get_buffer_info(env, item, (void **) &(buf->base), &(buf->len));
+  }
+
+  req->data = handle;
+  uv_write(req, handle, bufs, nbufs, on_write);
+
+  free(bufs);
 
   return NULL;
 }
+
+NAPI_METHOD(native_pipe_end) {
+  NAPI_ARGV(1)
+  NAPI_ARGV_BUFFER_CAST(native_pipe_t *, self, 0)
+
+  uv_shutdown_t *req = &(self->end);
+
+  req->data = self;
+  uv_shutdown(req, (uv_stream_t *) self, on_shutdown);
+
+  return NULL;
+}
+
+NAPI_METHOD(native_pipe_resume) {
+  NAPI_ARGV(1)
+  NAPI_ARGV_BUFFER_CAST(uv_stream_t *, handle, 0)
+
+  uv_read_start(handle, on_alloc, on_read);
+
+  return NULL;
+}
+
+NAPI_METHOD(native_pipe_pause) {
+  NAPI_ARGV(1)
+  NAPI_ARGV_BUFFER_CAST(uv_stream_t *, handle, 0)
+
+  uv_read_stop(handle);
+
+  return NULL;
+}
+
+NAPI_METHOD(native_pipe_close) {
+  NAPI_ARGV(1)
+  NAPI_ARGV_BUFFER_CAST(uv_handle_t *, handle, 0)
+
+  uv_close(handle, on_close);
+
+  return NULL;
+}
+
+// NAPI_METHOD(pipe_init) {
+//   NAPI_ARGV(1)
+
+//   uv_loop_t *loop;
+//   napi_get_uv_event_loop(env, &loop);
+
+//   NAPI_ARGV_BUFFER_CAST(uv_pipe_t *, handle, 0);
+
+//   uv_pipe_init(loop, handle, 0);
+
+//   return NULL;
+// }
+
+// NAPI_METHOD(pipe_open) {
+//   NAPI_ARGV(2)
+
+//   NAPI_ARGV_BUFFER_CAST(uv_pipe_t *, handle, 0);
+//   NAPI_ARGV_INT32(fd, 1);
+
+//   uv_pipe_open(handle, fd);
+
+//   return NULL;
+// }
+
+// NAPI_METHOD(pipe_write) {
+//   NAPI_ARGV(3)
+
+//   NAPI_ARGV_BUFFER_CAST(uv_write_t *, req, 0);
+//   NAPI_ARGV_BUFFER_CAST(uv_stream_t *, handle, 1);
+//   NAPI_ARGV_BUFFER(buf, 2);
+
+//   uv_buf_t bufs = {
+//     .base = buf,
+//     .len = buf_len
+//   };
+
+//   uv_write(req, handle, &bufs, 1, on_write);
+
+//   return NULL;
+// }
 // NAPI_METHOD(pipe_bind) {
 //   NAPI_ARGV(4)
 
@@ -55,7 +316,14 @@ NAPI_METHOD(pipe_write) {
 // }
 
 NAPI_INIT() {
-  NAPI_EXPORT_FUNCTION(pipe_init)
-  NAPI_EXPORT_FUNCTION(pipe_open)
-  NAPI_EXPORT_FUNCTION(pipe_write)
+  NAPI_EXPORT_SIZEOF(native_pipe_t);
+  NAPI_EXPORT_SIZEOF(uv_write_t)
+
+  NAPI_EXPORT_FUNCTION(native_pipe_init)
+  NAPI_EXPORT_FUNCTION(native_pipe_connect)
+  NAPI_EXPORT_FUNCTION(native_pipe_writev)
+  NAPI_EXPORT_FUNCTION(native_pipe_end)
+  NAPI_EXPORT_FUNCTION(native_pipe_resume)
+  NAPI_EXPORT_FUNCTION(native_pipe_pause)
+  NAPI_EXPORT_FUNCTION(native_pipe_close)
 }
--- index.js
+++ index.js
@@ -1,17 +1,151 @@
+const { Duplex } = require('streamx')
+const errors = require('uv-errors')
 const binding = require('node-gyp-build')(__dirname)
-const fs = require('fs')
 
-const handle = Buffer.alloc(65536)
-const req = Buffer.alloc(65536)
-const msg = Buffer.from('hello')
+const DEFAULT_READ_BUFFER = 65536
 
-const fd = fs.openSync('\\\\.\\pipe\\server')
+module.exports = class NativePipe extends Duplex {
+  constructor (path, { readBufferSize = DEFAULT_READ_BUFFER, allowHalfOpen = true } = {}) {
+    super({ mapWritable })
 
-binding.pipe_init(handle)
-binding.pipe_write(req, handle, msg)
+    this.fd = -1
 
-// console.log(fd)
+    const slab = Buffer.alloc(binding.sizeof_native_pipe_t + binding.sizeof_uv_write_t + readBufferSize)
 
-// binding.pipe_open(handle, 42)
+    this._handle = slab.subarray(0, binding.sizeof_native_pipe_t)
+    this._req = slab.subarray(binding.sizeof_native_pipe_t, binding.sizeof_native_pipe_t + binding.sizeof_uv_write_t)
+    this._buffer = slab.subarray(binding.sizeof_native_pipe_t + binding.sizeof_uv_write_t)
 
-// console.log(buf)
+    this._openCallback = null
+    this._writeCallback = null
+    this._finalCallback = null
+    this._destroyCallback = null
+
+    this._connected = 0 // unknown
+    this._allowHalfOpen = allowHalfOpen
+
+    binding.native_pipe_init(this._handle, this._buffer, this,
+      this._onconnect,
+      this._onwrite,
+      this._onfinal,
+      this._onread,
+      this._onclose
+    )
+
+    if (typeof path === 'number') {
+      binding.native_pipe_open(this._handle, path)
+      this._connected = 1
+      this.fd = path
+    } else {
+      this.fd = binding.native_pipe_connect(this._handle, path)
+    }
+  }
+
+  _open (cb) {
+    this._openCallback = cb
+    this._continueOpen()
+  }
+
+  _read (cb) {
+    binding.native_pipe_resume(this._handle)
+    cb(null)
+  }
+
+  _writev (datas, cb) {
+    this._writeCallback = cb
+    binding.native_pipe_writev(this._req, this._handle, datas)
+  }
+
+  _final (cb) {
+    this._finalCallback = cb
+    binding.native_pipe_end(this._handle)
+  }
+
+  _destroy (cb) {
+    this._destroyCallback = cb
+    binding.native_pipe_close(this._handle)
+  }
+
+  _continueOpen () {
+    if (this._connected === 0) return
+    if (this._openCallback === null) return
+    const cb = this._openCallback
+    this._openCallback = null
+    cb(this._connected < 0 ? makeError(this._connected) : null)
+  }
+
+  _continueWrite (err) {
+    if (this._writeCallback === null) return
+    const cb = this._writeCallback
+    this._writeCallback = null
+    cb(err)
+  }
+
+  _continueFinal (err) {
+    if (this._finalCallback === null) return
+    const cb = this._finalCallback
+    this._finalCallback = null
+    cb(err)
+  }
+
+  _continueDestroy () {
+    if (this._destroyCallback === null) return
+    const cb = this._destroyCallback
+    this._destroyCallback = null
+    cb(null)
+  }
+
+  _onconnect (status) {
+    this._connected = status < 0 ? status : 1
+    this._continueOpen()
+  }
+
+  _onwrite (status) {
+    const err = status < 0 ? makeError(status) : null
+    this._continueWrite(err)
+  }
+
+  _onread (read) {
+    if (read === 0) {
+      this.push(null)
+      if (this._allowHalfOpen === false) this.end()
+      return
+    }
+
+    if (read < 0) {
+      this.destroy(makeError(read))
+      return
+    }
+
+    const copy = Buffer.allocUnsafe(read)
+    copy.set(this._buffer.subarray(0, read))
+
+    if (this.push(copy) === false && this.destroying === false) {
+      binding.native_pipe_pause(this._handle)
+    }
+  }
+
+  _onfinal (status) {
+    const err = status < 0 ? makeError(status) : null
+    this._continueFinal(err)
+  }
+
+  _onclose () {
+    this._handle = null
+    this._continueDestroy()
+  }
+}
+
+function mapWritable (buf) {
+  return typeof buf === 'string' ? Buffer.from(buf) : buf
+}
+
+function makeError (errno) {
+  const [code, msg] = errors.get(errno)
+  const err = new Error(code + ': ' + msg)
+
+  err.code = code
+  err.errno = errno
+
+  return err
+}
